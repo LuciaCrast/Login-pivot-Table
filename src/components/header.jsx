ce32ef96e4ca5415c82165f2adb1c507c88a4c85
Lucia Cano <luciacanoras@gmail.com>
2022-03-18 15:14:47 +0100

    Add header and header styling

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import "./header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title"> NoNameWeb</h1>
+    </header>
+  );
+}
+
+export default Header;
